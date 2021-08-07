6593dae0c7f3d2b246bb5b2c60c54d1249f9b1ed
whatsmysuggestion <whatsmysuggestion@gmail.com>
2021-08-07 23:27:02 +0530

    ngIF-Directive-Using-Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  Title: string = 'Welcome To';
+  Status: boolean = false;
+}
